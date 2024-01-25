21a035c3b1dca71dc7b9332b64e7814009807b2b
Olexandr Minzak <coding@bizlevel.net>
2024-01-25 20:16:40 +0100

    Add results to readme

--- q.sh
+++ q.sh
@@ -2,4 +2,4 @@ sqlite3 -echo -box data.db 'SELECT * FROM Dosar WHERE result=True AND year=2023;
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="48275/RD/2023";'
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="49000/RD/2023";'
 
-sqlite3 -box data.db '.read stat.sql'
+sqlite3 -echo -box data.db '.read stat.sql'
