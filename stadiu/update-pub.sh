0ecfcc63f3fbb2a9bfbf5453db315a43c4be86d2
Olexandr Minzak <coding@bizlevel.net>
2024-03-25 15:05:46 +0100

    Update march 2024

--- stadiu/update-pub.sh
+++ stadiu/update-pub.sh
@@ -2,5 +2,5 @@
 
 mkdir pub/`date +%Y-%m-%d` 2>/dev/null
 for year in `seq 2015 2024`; do
-    wget -q --show-progress -P pub/`date +%Y-%m-%d` "https://cetatenie.just.ro/wp-content/uploads/2024/11/Art.-11-$year-Redobandire.pdf"
+    wget -q --show-progress -P pub/`date +%Y-%m-%d` "https://cetatenie.just.ro/wp-content/uploads/2023/11/Art.-11-$year-Redobandire.pdf"
 done
