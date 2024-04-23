7144e8cac927e6e1fc16d5813ae53ee0af6f17d4
Olexandr Minzak <coding@bizlevel.net>
2024-04-23 10:37:20 +0200

    Fix via skipping bad date in Ordin-920-din-25.04.2024-art-11.pdf

--- get_ordins.py
+++ get_ordins.py
@@ -9,17 +9,17 @@ from bs4 import BeautifulSoup
 Ordins = './ordins/'
 
 OrdineUrl = "https://cetatenie.just.ro/ordine-articolul-1-1/"
-Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'}
+DownloadUrl = 'https://cetatenie.just.ro/storage/'
+#Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'}
+Headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'}
 r = requests.get(OrdineUrl, headers=Headers)
 
-# Проходимся по всем ссылкам со страницы с приказами по 11 артикулу https://cetatenie.just.ro/ordine-articolul-1-1/
-# Выкачиваем все ссылки, которые содержат в адресе cetatenie.just.ro/wp-content/uploads
+# Проходимся по всем ссылкам со страницы с приказами по 11 артикулу OrdineUrl
+# Выкачиваем все ссылки, которые содержат в адресе DownloadUrl
 soup = BeautifulSoup(r.text, 'html.parser')
-#for link in soup.find_all('a', href=re.compile('cetatenie.just.ro/wp-content/uploads')):
-for link in soup.find_all('a', href=re.compile('cetatenie.just.ro/storage')):
+for link in soup.find_all('a', href=re.compile(DownloadUrl)):
     OrdineUrl = link.get('href')
-#    FileName = Ordins+OrdineUrl.replace('https://cetatenie.just.ro/wp-content/uploads/', '').replace('/', '-')
-    FileName = Ordins+OrdineUrl.replace('https://cetatenie.just.ro/storage/', '').replace('/', '-')
+    FileName = Ordins+OrdineUrl.replace(DownloadUrl, '').replace('/', '-')
     if not os.path.isfile(FileName):
         r = requests.get(OrdineUrl, headers=Headers)
         if r.status_code == 200:
--- update.sh
+++ update.sh
@@ -3,9 +3,12 @@
 #python3 ./parse_stadiu.py
 
 rm -f ordins/.gitkeep
-rm -f *.log
 python3 ./get_ordins.py
+#https://cetatenie.just.ro/storage/2023/11/Ordin-920-din-25.04.2024-art-11.pdf
+rm -f ordins/2023-11-Ordin-920-din-25.04.2024-art-11.pdf
 python3 ./parse_ordins.py
+python3 ./get_ordins.py
 tree -L 5 -I 'venv|*.log' > tree.txt
 ./q.sh > q.txt
 touch ordins/.gitkeep
+rm -f *.log
