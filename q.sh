0296f46821bdcd7704b8390a31bd0d261fea7ae7
Olexandr Minzak <coding@bizlevel.net>
2024-07-12 14:57:25 +0200

    Udate

--- q.sh
+++ q.sh
@@ -1,9 +1,13 @@
 sqlite3 -echo -box data.db 'SELECT * FROM Dosar WHERE result=True AND year=2024;'
 sqlite3 -echo -box data.db 'SELECT * FROM Dosar WHERE result=True AND year=2023;'
 
+#https://t.me/Yuliya_pm
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="48275/RD/2023";'
+#https://t.me/ProAdmin
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="49000/RD/2023";'
+#https://t.me/msgme1
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="14833/RD/2024";'
+#https://t.me/vorkos
 sqlite3 -echo -box data.db 'SELECT * from Dosar where id="38286/RD/2021";'
 
 sqlite3 -echo -box data.db '.read stat.sql'
--- update-ording.sh
+++ update-ording.sh
@@ -4,11 +4,16 @@
 
 rm -f ordins/.gitkeep
 python3 ./get_ordins.py
-#https://cetatenie.just.ro/storage/2023/11/Ordin-920-din-25.04.2024-art-11.pdf
+
 rm -f ordins/2023-11-Ordin-920-din-25.04.2024-art-11.pdf
+rm -f ordins/2023-11-Ordin-1097-P-2024-din-26-06-2024-Art-11-NPE.pdf
 python3 ./parse_ordins.py
 python3 ./get_ordins.py
 tree -L 5 -I 'venv|*.log' > tree.txt
 ./q.sh > q.txt
 touch ordins/.gitkeep
 rm -f *.log
+
+###
+#wget https://cetatenie.just.ro/storage/2023/11/Ordin-1095-din-19-06-2024-art11.pdf -O 2023-11-Ordin-1095-din-19-06-2024-art11.pdf
+#wget https://cetatenie.just.ro/storage/2023/11/Ordin-1097-P-2024-din-26-06-2024-Art-11-NPE.pdf -O 2023-11-Ordin-1097-P-2024-din-26-06-2024-Art-11-NPE.pdf
